b0414e2c49ea66e07b3d73665420d758366957f9
Marín Salinas <marinssalinas@gmail.com>
2015-03-24 00:38:09 -0600

    Cable Test Done

--- src/com/verificacion/unitTest/CableTest.java
+++ src/com/verificacion/unitTest/CableTest.java
@@ -4,31 +4,97 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.verificacion.pruebas.Cable;
+
 public class CableTest {
 
 	@Test
 	public void testRegresaDatos() {
-		fail("Not yet implemented");
+		String nombreCliente = "Jose Gamez";
+		int numCuenta = 13;
+		float saldoTotal = 200;
+		int edad = 22;
+		int paquete = 3;
+		int numTv = 3;
+		
+		
+		Cable cable = new Cable(nombreCliente, numCuenta, saldoTotal, edad, paquete, numTv);
+		assertEquals("El cliente "+nombreCliente+", con número de cuenta # "+numCuenta+", tiene un saldo de internet de: "+saldoTotal+".", cable.regresaDatos());
+		
 	}
 
-	@Test
+	@Test(expected=IllegalArgumentException.class)
 	public void testCable() {
-		fail("Not yet implemented");
+		String nombreCliente = "Miguel Sanz";
+		int numCuenta = 13;
+		float saldoTotal = 200;
+		int edad = 14;
+		int paquete = 0;
+		int numTv = 2;
+		
+		
+		Cable cable = new Cable(nombreCliente, numCuenta, saldoTotal, edad, paquete, numTv);
+		
 	}
 
 	@Test
 	public void testGetSaldo() {
-		fail("Not yet implemented");
+		String nombreCliente = "Carlos K";
+		int numCuenta = 33;
+		float saldo = 200;
+		int edad = 20;
+		int paquete = 2;
+		int numTv = 5;
+		
+		
+		Cable cable = new Cable(nombreCliente, numCuenta, saldo, edad, paquete, numTv);
+		assertEquals(saldo, cable.getSaldo(), 0);
 	}
 
 	@Test
 	public void testNombrePaquete() {
-		fail("Not yet implemented");
+		String nombreCliente = "Klinsmann";
+		int numCuenta = 13;
+		float saldo = 500;
+		int edad = 21;
+		int paquete = 6;
+		int numTv = 5;
+		
+		
+		Cable cable = new Cable(nombreCliente, numCuenta, saldo, edad, paquete, numTv);
+		System.out.println(cable.nombrePaquete());
 	}
 
 	@Test
 	public void testCostoMensualTV() {
-		fail("Not yet implemented");
+		String nombreCliente = "Ludim";
+		int numCuenta = 666;
+		float saldo = 50;
+		int edad = 22;
+		int paquete = 2;
+		int numTv = 5;
+		
+		
+		Cable cable = new Cable(nombreCliente, numCuenta, saldo, edad, paquete, numTv);
+		assertEquals("XXX", cable.nombrePaquete());
+	
+		
 	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testCostoMensualTVMenorEdad(){
+		
+		String nombreCliente = "Juan";
+		int numCuenta = 7;
+		int saldo = 3;
+		int edad = 17;
+		int paquete = 2;
+		int numTv = 2;
+		
+		
+		Cable cable2 = new Cable(nombreCliente, numCuenta, saldo, edad, paquete, numTv);
+		System.out.println(cable2.nombrePaquete());
+	}
+	
 
 }
